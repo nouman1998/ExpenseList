880430393d4d43b1a2e10c787df8c973ed3d3172
nouman1998 <nouman.ejaz198@gmail.com>
2020-07-02 13:30:04 +0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import  {Expense} from './expense';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -17,11 +18,39 @@ export class AppComponent {
   ) {
     this.initializeApp();
   }
+  
+  reasonInput;
+  amountInput;
+  expenseList = []
+  totalExpenses:number=0;
 
+
+  ngOnInit() {
+    this.expenseList = [];
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  addExpense() {
+   let expense: Expense = new Expense();
+    expense.amount = this.amountInput;
+    expense.reason = this.reasonInput;
+    this.totalExpenses+=parseInt(this.amountInput);
+
+    this.expenseList.push(expense);
+    console.log(this.expenseList);
+
+
+   
+    this.reasonInput="";
+    this.amountInput="";
+  }
+
+  clearList(){
+    this.expenseList=[];
+    
   }
 }
